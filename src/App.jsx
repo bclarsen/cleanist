9c860aa25ee765abe3d09712c94e1a25787af178
bclarsen <benclarsen405@gmail.com>
2026-07-16 14:07:52 -0600

    Add testing line in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,8 @@ import TaskForm from './components/TaskForm';
 import TaskList from './components/TaskList';
 import StatsPanel from './components/StatsPanel';
 
+// testing
+
 const tasksRef = collection(db, 'tasks');
 const invitesRef = collection(db, 'teamInvites');
 
